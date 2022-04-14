54f4986653e9c3c7afc7dc7ee0696f4addc2bbb8
Lauffret <auffretlea@orange.fr>
2022-04-14 19:53:32 +0200

    ajout todo

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import Todo from "./Todo";
 import {v1 as uuid} from "uuid";
 
 const Todolist = () => {
@@ -8,12 +9,52 @@ const Todolist = () => {
         { id: uuid(), task: "tâche 2", completed: true },
         { id: uuid(), task: "tâche 3", completed: false }
       ]);
+
+      const create = newTodo => {
+        console.log(newTodo);
+        setTodos([...todos, newTodo]);
+      };
+    
+      const remove = id => {
+        setTodos(todos.filter(todo => todo.id !== id));
+      };
+    
+      const update = (id, updtedTask) => {
+        const updatedTodos = todos.map(todo => {
+          if (todo.id === id) {
+            return { ...todo, task: updtedTask };
+          }
+          return todo;
+        });
+        setTodos(updatedTodos);
+      };
+    
+      const toggleComplete = id => {
+        const updatedTodos = todos.map(todo => {
+          if (todo.id === id) {
+            return { ...todo, completed: !todo.completed };
+          }
+          return todo;
+        });
+        setTodos(updatedTodos);
+      };
+    
+      const todosList = todos.map(todo => (
+        <Todo
+          toggleComplete={toggleComplete}
+          update={update}
+          remove={remove}
+          key={todo.id}
+          todo={todo}
+        />
+      ));
     
       return (
         <div className="TodoList">
           <h1>
             Todo List
           </h1>
+          <ul>{todosList}</ul>
         </div>
       );
 };
